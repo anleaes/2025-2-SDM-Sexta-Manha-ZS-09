561cd323f3d09b95323d8bfd329a9cb0a6e16bb2
Joao Pedro <joaopedrostroppermarques02@gmail.com>
2025-11-20 13:59:42 -0300

    criação do app disciplinas

---
+++ gestaoescolar/apps/disciplinas/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DisciplinasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'disciplinas'
+    verbose_name = 'Disciplinas'

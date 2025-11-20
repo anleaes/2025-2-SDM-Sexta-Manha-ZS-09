878d8314f0d458fcc5c03360f3d4eb331bc0877b
Joao Pedro <joaopedrostroppermarques02@gmail.com>
2025-11-20 14:50:44 -0300

    configuração serializer a view

--- gestaoescolar/apps/professores/views.py
+++ gestaoescolar/apps/professores/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Professor
+from .serializer import ProfessorSerializer
 
 # Create your views here.
+
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer

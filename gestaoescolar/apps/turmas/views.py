da44747094555732ddb5760e6efe4787990e99ed
Joao Pedro <joaopedrostroppermarques02@gmail.com>
2025-11-20 15:30:17 -0300

    configurando o view

--- gestaoescolar/apps/turmas/views.py
+++ gestaoescolar/apps/turmas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Turma
+from .serializer import TurmaSerializer
 
 # Create your views here.
+
+class TurmaViewSet(viewsets.ModelViewSet):
+    queryset = Turma.objects.all()
+    serializer_class = TurmaSerializer

e2d173daccbad700999a9c19e493a7a86417fa55
Joao Pedro <joaopedrostroppermarques02@gmail.com>
2025-11-20 15:45:54 -0300

    configurando o views

--- gestaoescolar/apps/alunos/views.py
+++ gestaoescolar/apps/alunos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Aluno
+from .serializer import AlunoSerializer
 
 # Create your views here.
+
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer

ce81ffd0bf0200a32812aa31a594cb591002708c
Joao Pedro <joaopedrostroppermarques02@gmail.com>
2025-11-20 15:14:59 -0300

    configuração do views

--- gestaoescolar/apps/salas/views.py
+++ gestaoescolar/apps/salas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Sala
+from .serializer import SalaSerializer
 
 # Create your views here.
+
+class SalaViewSet(viewsets.ModelViewSet):
+    queryset = Sala.objects.all()
+    serializer_class = SalaSerializer

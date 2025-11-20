9335f2542afc6b4cea83a4e038b88582948df482
Joao Pedro <joaopedrostroppermarques02@gmail.com>
2025-11-20 16:02:08 -0300

    definindo o verbose_name

--- gestaoescolar/apps/matriculas/apps.py
+++ gestaoescolar/apps/matriculas/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MatriculasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'matriculas'
+    verbose_name = 'Matrículas'

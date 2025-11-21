a0686713d40d297aee98c87ac47b5beee0a100b4
Joao Pedro <joaopedrostroppermarques02@gmail.com>
2025-11-21 09:10:42 -0300

    protegendo as rotas de alunos

--- gestaoescolar/apps/alunos/views.py
+++ gestaoescolar/apps/alunos/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Aluno
 from .serializer import AlunoSerializer
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class AlunoViewSet(viewsets.ModelViewSet):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
